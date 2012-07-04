4302375a13721c2ee1e2d1c6754b2a1969869d43
Travis Berry <travisberry@travisberry.com>
2012-07-04 16:48:59 -0600

    removing set width

--- vendor/assets/javascripts/select2.js
+++ vendor/assets/javascripts/select2.js
@@ -733,8 +733,8 @@
             var width  = this.container.outerWidth();
             var css    = {
                 top: offset.top + height,
-                left: offset.left
-                //width: width
+                left: offset.left,
+                width: width
             }
             this.dropdown.css(css);
         },
